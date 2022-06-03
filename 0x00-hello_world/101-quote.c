aeff405ee04ae8cef6173b36ca29b3344f8578b1
root <nabwiremaryann7@gmail.com>
2022-06-03 16:12:10 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - main function in the task
  *
- * Return: Always 0 (Success)
+ *
+ * Return: return 1
  */
 int main(void)
 {
