5a847d525676d44413078ec3379265b0c6c321ad
root <nabwiremaryann7@gmail.com>
2022-06-07 08:23:24 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/** 
+ * main - prints _putchar
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('P');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
